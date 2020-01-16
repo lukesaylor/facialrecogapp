0695f23e9fcbf23d84df586649c3f50284ab9cd7
luke <luke@Cheris-MacBook-Pro.local>
2020-01-16 12:50:46 -0500

    background gradient added

---
+++ my-app/src/Components/Logo /Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <p>helo</p>
+        </div>
+    )
+};
+
+export default Logo;
