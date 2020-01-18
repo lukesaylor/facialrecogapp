62793016d4305fb3ed93f15fbecfbb2027add2cc
luke <luke@Cheris-MacBook-Pro.local>
2020-01-18 14:22:34 -0500

    logo added

--- my-app/src/Components/Logo/Logo.js
+++ my-app/src/Components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css';
+import brain from './brain.png'
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 40 }} style={{ height: 150, width: 150 }} >
-            <div className="Tilt-inner"> 👽 </div>
+            <div className="Tilt-inner"> <img style={{padding: '25px'}} alt='brain logo' src={brain} /> </div>
             </Tilt>
         </div>
     )
