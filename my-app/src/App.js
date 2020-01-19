49ba9096fec917edeb338ede4d3120e828296b1c
Lukas Saylor <slappyhoursound@gmail.com>
2020-01-18 19:01:09 -0500

    particle background added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo.js';
-import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particleoptions = {
+  particles: {
+    number: {
+      value:100,
+      density: {
+        enable: true,
+        value_area: 800,
+        blur: 5
+      }
+    }
+  }
+}
+
+
+
+
 
 class App extends Component {
   render () {
     return (
       <div className="App">
+        <Particles className='particles'
+                params={particleoptions} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
       </div>
     );
