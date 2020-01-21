94c98b25b41a79138b861d781532042aac75339c
luke <luke@Cheris-MacBook-Pro.local>
2020-01-21 15:29:23 -0500

    API functionality

--- my-app/src/Components/FaceRecognition/FaceRecognition.js
+++ my-app/src/Components/FaceRecognition/FaceRecognition.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ imageUrl }) => {
     return (
         <div className='center'>
-           <img alt='' src={'https://samples.clarifai.com/face-det.jpg'}/>
+           <img alt='' src={imageUrl}/>
         </div>
     )
 };
