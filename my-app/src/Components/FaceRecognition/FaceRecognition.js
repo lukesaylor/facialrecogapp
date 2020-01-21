3a82d4b3e63edf35f010f127414a7884380a81fa
luke <luke@Cheris-MacBook-Pro.local>
2020-01-21 13:57:50 -0500

    facerecog testing

--- my-app/src/Components/FaceRecognition/FaceRecognition.js
+++ my-app/src/Components/FaceRecognition/FaceRecognition.js
@@ -4,8 +4,8 @@ import React from 'react';
 
 const FaceRecognition = () => {
     return (
-        <div>
-           
+        <div className='center'>
+           <img alt='' src={'https://samples.clarifai.com/face-det.jpg'}/>
         </div>
     )
 };
